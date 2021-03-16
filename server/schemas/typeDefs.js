1c1a0b8d9acc1ee2dcee92f8b1bb6b40ff7c7f23
shaggy <simmons5family@gmail.com>
2021-03-16 18:18:32 -0500

    add create user, login route and json web tokens. all working

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,6 +6,11 @@ const { gql } = require('apollo-server-express');
 // create our typeDefs
 const typeDefs = gql`
 
+    type Auth {
+        token: ID!
+        user: User
+    }
+
     type Thought {
         _id: ID
         thoughtText: String
@@ -37,7 +42,12 @@ const typeDefs = gql`
         thoughts(username: String): [Thought]
         thought(_id: ID!): Thought
     }
-`;
 
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth 
+    }
+`;
+                                                            // User replaced with Auth
 // export the typeDefs
 module.exports = typeDefs;
