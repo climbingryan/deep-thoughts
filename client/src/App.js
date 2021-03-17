e8811f6333adeec1301ba56f22749bcd0cba902b
shaggy <simmons5family@gmail.com>
2021-03-17 17:43:29 -0500

    add Thoughts query to client in utils

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+// special react component we'll use to provide data to all other components
+import { ApolloProvider } from '@apollo/react-hooks';
+// Used to get data when we're ready to use it
+import ApolloClient from 'apollo-boost';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+
+// new connection to GraphQl using Apollo
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql'
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className='flex-column justify-flex-start min-100-vh'>
+        <Header />
+        <div className='container'>
+          <Home />
+        </div>
+        <Footer />
+    </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
