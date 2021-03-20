c5e935b1f7079cd21911c2275c41cd1e80921e1a
shaggy <simmons5family@gmail.com>
2021-03-20 00:39:29 -0500

    add user authentication, friends tab in home, and features for better UX

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,15 @@ import Home from './pages/Home';
 
 // new connection to GraphQl using Apollo
 const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
   uri: '/graphql'
 });
 // Uniform Resource identifier
